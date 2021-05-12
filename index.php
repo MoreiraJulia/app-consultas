34cc0a66fd80e685434fc474fd0b1f9156c98445
Julia Moreira <julia.aparecida25@gmail.com>
2021-05-11 21:48:56 -0300

    Inclusão do bootstrap, jquery, página cabecalho e rodape

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>APP Consultas</title>
-</head>
-<body>
-    <main>
-        <header>
-            <h1>APP Consultas</h1>
-            <hr>
-        </header>
-        <nav>
-            <a href="#"><img src="img/medico.png">Cadastro de médicos</a>
-            <a href="#"><img src="img/paciente1.png">Cadastro de pacientes</a>
-            <a href="#"><img src="img/calendario.png">Agendamento de consultas</a>
-        </nav>
+<?php include "cabecalho.php"; ?>
         <div class="conteudo">
             <h2>Conteúdo da aplicação</h2>
         </div>
-        <footer>
-            &copy; Todos os direitos reservados
-        </footer>
-    </main>
-    
-</body>
-</html>
+<?php include "rodape.php"; ?>
---
+++ rodape.php
@@ -0,0 +1,8 @@
+<footer>
+            &copy; Todos os direitos reservados
+        </footer>
+    </main>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.js"></script>
+</body>
+</html>
