984c2209b062c0b1fe6eae1c03f9f7efe49ba9a2
Julia Moreira <julia.aparecida25@gmail.com>
2021-05-13 22:32:06 -0300

    Criação das pastas includes,  medicos, e das páginas de funcionalidade de cadastro de médicos

--- rodape.php
+++
@@ -1,8 +0,0 @@
-<footer>
-            &copy; Todos os direitos reservados
-        </footer>
-    </main>
-    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery.js"></script>
-</body>
-</html>
