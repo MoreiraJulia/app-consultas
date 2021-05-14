3166158d0fa0fa92238608eb06f4b4b71ed851af
Julia Moreira <julia.aparecida25@gmail.com>
2021-05-14 19:50:45 -0300

    Atualização da página medicos-formulario-inserir

--- medicos/medicos-formulario-inserir.php
+++ medicos/medicos-formulario-inserir.php
@@ -1,26 +1,28 @@
 <?php include "../includes/cabecalho.php"; ?>
 
-<form name="formulario-inserir-medicos" method="post" action="medicos-inserir.php">
-    <p>
-        <label>Nome:</label><input name="nome">
+<form name="formulario-inserir-medicos" class="mt-5" method="post" action="medicos-inserir.php">
+<div class="row">
+    <p class="col-md-6">
+        <label class="form-label">Nome:</label><input class="form-control" name="nome">
     </p>
-    <p>
-        <label>Telefone:</label><input name="telefone">
+    <p class="col-md-6">
+        <label class="form-label">Telefone:</label><input class="form-control" name="telefone">
     </p>
-    <p>
-        <label>CRM:</label><input name="crm">
+    <p class="col-md-6">
+        <label class="form-label">CRM:</label><input class="form-control" name="crm">
     </p>
-    <p>
+    <p class="col-6">
         <label>Especialidade:</label>
-        <select name="especialidade">
+        <select name="especialidade" class="form-select" aria-label="Default select example">
             <option value="Cardiologista">Cardiologista</option>
             <option value="Nutricionista">Nutricionista</option>
             <option value="Ortopedista">Ortopedista</option>
         </select>
     </p>
-    <p>
-        <button type="submit">Salvar</button>
+    <p class="d-md-flex justify-content-md-end mt-2">
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </p>
+    </div>
 </form>
 
 <?php include "../includes/rodape.php"; ?>
